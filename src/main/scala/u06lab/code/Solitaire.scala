2ffdcd9ad8c09c4f8fa22eea7e52eeca4c67f31a
Andrea Negri <andrea.negri4@studio.unibo.it>
2023-04-28 10:26:58 +0200

    Completato es3

--- src/main/scala/u06lab/code/Solitaire.scala
+++ src/main/scala/u06lab/code/Solitaire.scala
@@ -1,42 +1,45 @@
 package u06lab.code
 
 object Solitaire extends App:
-  def render(solution: Seq[(Int, Int)], width: Int, height: Int): String =
+  def render(solution: Seq[(Int, Int)], size: (Int, Int)): String =
     val reversed = solution.reverse
     val rows =
-      for y <- 0 until height
-          row = for x <- 0 until width
+      for y <- 0 until size._2
+          row = for x <- 0 until size._1
           number = reversed.indexOf((x, y)) + 1
           yield if number > 0 then "%-2d ".format(number) else "X  "
       yield row.mkString
     rows.mkString("\n")
 
-
-  val solutions = placeMarks(5, 5)((2, 2))
-  for s <- solutions
-    do println(render(solution = s, width = 5, height = 5) + "\n")
-  println(solutions.size)   // 13272
-
-
   type Position = (Int, Int)
   type Solution = List[Position]
 
-  def placeMarks(width: Int, height: Int)(init: Position): Iterable[Solution] =
+  def placeMarks(size: (Int, Int))(init: Position): Iterable[Solution] =
     def _placeMarks(n: Int): Iterable[Solution] = n match
       case 1 => Seq(List(init)).view
       case _ =>
         for
           solution <- _placeMarks(n - 1)
-          i <- 0 until width
-          j <- 0 until height
+          i <- 0 until size._1
+          j <- 0 until size._2
           newPos = (i, j)
           if !solution.contains(newPos)
           if isSafe(newPos, solution.head)
         yield newPos :: solution
 
-    _placeMarks(width * height)
+    _placeMarks(size._1 * size._2)
 
   def isSafe(p1: Position, p2: Position): Boolean =
     (math.abs(p1._1 - p2._1) == 2 && math.abs(p1._2 - p2._2) == 2)
       || (math.abs(p1._1 - p2._1) == 3 && math.abs(p1._2 - p2._2) == 0)
       || (math.abs(p1._1 - p2._1) == 0 && math.abs(p1._2 - p2._2) == 3)
+
+  val size = (5, 5) //._1 width ._2 height
+  val center = (size._1 / 2, size._2 / 2)
+  //println(center)
+  val solutions = placeMarks(size)(center)
+  if (true) //
+    for s <- solutions
+      do println(render(s, size) + "\n")
+
+  println(solutions.size)
